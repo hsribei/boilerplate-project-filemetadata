57829175e6ea51dc836745e7b30fbe8845590558
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-03-11 19:05:24 -0400

    Cosmetics

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
+var express = require("express");
+var cors = require("cors");
 
 // require and use "multer"...
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.get("/hello", function(req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log(`Node listening on port ${port}...`);
 });
