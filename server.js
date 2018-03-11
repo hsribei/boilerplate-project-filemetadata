69c2d94a099db23ea47e94c035ffebf7132f5811
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-03-11 19:30:36 -0400

    Get rid of /hello endpoint

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ app.get("/", function(req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.get("/hello", function(req, res) {
-  res.json({ greetings: "Hello, API" });
-});
-
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log(`Node listening on port ${port}...`);
